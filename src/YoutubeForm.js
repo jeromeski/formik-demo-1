7b1d68d9c89678eb41fa742e0017dc718a3f58eb
Jerome Gacoscosim <40538528+jeromeski@users.noreply.github.com>
2021-06-28 10:46:17 +0800

    Field Component

--- src/YoutubeForm.js
+++ src/YoutubeForm.js
@@ -1,4 +1,4 @@
-import {Formik, Form} from 'formik';
+import {Formik, Form, Field} from 'formik';
 import * as Yup from 'yup';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -34,33 +34,30 @@ function YoutubeForm() {
       <Form>
         <div className="form-control">
           <label htmlFor="name">Name:</label>
-          <input 
+          <Field 
             type="text" 
             name="name" 
             id="name"
-            {...formik.getFieldProps('name')}
           />
           {formik.touched.name && formik.errors.name ? <div className="error">{formik.errors.name}</div>: null}
         </div>
 
         <div className="form-control">
           <label htmlFor="email">Email:</label>
-          <input 
+          <Field 
             type="email" 
             name="email" 
             id="email"
-            {...formik.getFieldProps('email')}
           />
           {formik.touched.email && formik.errors.email ? <div className="error">{formik.errors.email}</div>: null}
         </div>
 
         <div className="form-control">
           <label htmlFor="channel">Channel:</label>
-          <input 
+          <Field 
             type="text" 
             name="channel" 
             id="channel"
-            {...formik.getFieldProps('channel')}
           />
           {formik.touched.channel && formik.errors.channel ? <div className="error">{formik.errors.channel}</div>: null}
         </div>
